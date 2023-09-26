bdd1f0b09d48b04363593de67fc69e362824c1cc
fauzaanu <fauzaangasim@gmail.com>
2023-09-26 07:00:38 +0500

    sample app displays todays date

--- djangoExample/settings.py
+++ djangoExample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1#)34^i^2aqzv7shm8z8-8n1-lv-xh^q5zq*u*ths&%*g@1w*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sampleapp.apps.SampleappConfig',
 ]
 
 MIDDLEWARE = [
